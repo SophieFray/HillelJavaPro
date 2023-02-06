7778ff8808387ad8b4a5dfc32c54afd70770fdd0
Muda Muda <mr,acksel@gmail.com>
2023-02-06 11:43:04 +0200

    Homework 3 done

---
+++ src/main/java/homework3/coworker/Main.java
@@ -0,0 +1,11 @@
+package src.main.java.homework3.coworker;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Coworker coworker1 = new Coworker("Alex", "QA", "mr.acksel@gmail.com","+38 063 697 59 16", 27);
+
+    }
+
+}
---
+++ src/main/java/homework3/sameNamePackage2/SameName.java
@@ -0,0 +1,7 @@
+package src.main.java.homework3.sameNamePackage2;
+
+public class SameName {
+
+
+
+}
