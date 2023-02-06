ae99a32a93af8dac8002d9b7cde131e1aec7ed32
Oleksandr <mr.acksel@gmaill.com>
2023-02-06 19:00:03 +0200

    homework 4 fixes - cleared animal counter

--- src/main/java/homework4/Animals.java
+++ src/main/java/homework4/Animals.java
@@ -2,8 +2,6 @@ package src.main.java.homework4;
 
 public abstract class Animals {
 
-    private static int animalsCounter = 0;
-
     public abstract void run(int runLengths);
     public abstract void swim(int swimLengths);
 
